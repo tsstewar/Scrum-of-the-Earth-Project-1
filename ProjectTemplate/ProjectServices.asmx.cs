0faa4d269ecfe8a676e08666059c82ccbb5fdab6
Tanner Stewart <tsstewar@sundevils.asu.edu>
2020-02-27 09:56:54 -0700

    edited the database, added a foreign key from users to usertimes to help us with posting the time data, added first and last name to database and create account page.

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -121,14 +121,14 @@ namespace ProjectTemplate
 		
 		//EXAMPLE OF AN INSERT QUERY WITH PARAMS PASSED IN.  BONUS GETTING THE INSERTED ID FROM THE DB!
 		[WebMethod(EnableSession = true)]
-		public void RequestAccount(string userid, string pass)
+		public void RequestAccount(string userid, string pass, string firstname, string lastname)
 		{
 			//string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
 			string sqlConnectString = getConString();
 			//the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
 			//does is tell mySql server to return the primary key of the last inserted row.
-			string sqlSelect = "insert into users (userid, pass) " +
-				"values(@userIdValue, @passValue); SELECT LAST_INSERT_ID();";
+			string sqlSelect = "insert into users (userid, pass, firstname, lastname) " +
+				"values(@userIdValue, @passValue, @firstnameValue, @lastnameValue); SELECT LAST_INSERT_ID();";
 
 			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
 			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
@@ -136,13 +136,15 @@ namespace ProjectTemplate
 			//sqlCommand.Parameters.AddWithValue("@idNumValue", HttpUtility.UrlDecode(id));
 			sqlCommand.Parameters.AddWithValue("@userIdValue", HttpUtility.UrlDecode(userid));
 			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+            sqlCommand.Parameters.AddWithValue("@firstnameValue", HttpUtility.UrlDecode(firstname));
+            sqlCommand.Parameters.AddWithValue("@lastnameValue", HttpUtility.UrlDecode(lastname));
 
 
-			//this time, we're not using a data adapter to fill a data table.  We're just
-			//opening the connection, telling our command to "executescalar" which says basically
-			//execute the query and just hand me back the number the query returns (the ID, remember?).
-			//don't forget to close the connection!
-			sqlConnection.Open();
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
 			//we're using a try/catch so that if the query errors out we can handle it gracefully
 			//by closing the connection and moving on
 			try
@@ -158,5 +160,15 @@ namespace ProjectTemplate
 			}
 			sqlConnection.Close();
 		}
-	}
+
+        [WebMethod(EnableSession = true)]
+        public bool LogOff()
+        {
+            //if they log off, then we remove the session.  That way, if they access
+            //again later they have to log back on in order for their ID to be back
+            //in the session!
+            Session.Abandon();
+            return true;
+        }
+    }
 }
