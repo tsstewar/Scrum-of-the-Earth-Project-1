d74571a9ca590600ce7678f4cb3f43a45f48ac5a
Tanner Stewart <tsstewar@sundevils.asu.edu>
2020-02-27 13:53:27 -0700

    created a users class needed to populate the users page

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -160,15 +160,5 @@ namespace ProjectTemplate
 			}
 			sqlConnection.Close();
 		}
-
-        [WebMethod(EnableSession = true)]
-        public bool LogOff()
-        {
-            //if they log off, then we remove the session.  That way, if they access
-            //again later they have to log back on in order for their ID to be back
-            //in the session!
-            Session.Abandon();
-            return true;
-        }
     }
 }
