1d6e35b5251c83d1b80091f7577a2adb893d158f
ychung25 <ychung25@asu.edu>
2020-03-01 16:42:24 -0700

    Added timer DB connection; doesn't work

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -203,6 +203,44 @@ namespace ProjectTemplate
 			sqlConnection.Close();
 		}
 
+        [WebMethod(EnableSession = true)]
+        public void AddTime(string project_name, string time_logged)
+        {
+            //string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            string sqlConnectString = getConString();
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = "insert into users_times (project_name, time_logged) " +
+                "values(@project_nameValue, @time_loggedValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //sqlCommand.Parameters.AddWithValue("@idNumValue", HttpUtility.UrlDecode(id));
+            sqlCommand.Parameters.AddWithValue("@project_nameValue", HttpUtility.UrlDecode(project_name));
+            sqlCommand.Parameters.AddWithValue("@time_loggedValue", HttpUtility.UrlDecode(time_logged));
+
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
 
 
 
@@ -210,8 +248,7 @@ namespace ProjectTemplate
 
 
 
-
-		[WebMethod(EnableSession = true)]
+        [WebMethod(EnableSession = true)]
 		public bool LogOff()
 		{
 			//if they log off, then we remove the session.  That way, if they access
