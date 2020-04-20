e5523fd0a110fdd64167ec8a128d79d8b4202184
Sarah Garbinski <sgarbins@asu.edu>
2020-04-19 21:06:45 -0700

    added stuff to notifications

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -135,75 +135,6 @@ namespace ProjectTemplate
             }
             sqlConnection.Close();
         }
-        [WebMethod(EnableSession = true)]
-        public void CreateProject(string project_name, string billable_rate)
-        {
-            //string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-            string sqlConnectString = getConString();
-            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
-            //does is tell mySql server to return the primary key of the last inserted row.
-            string sqlSelect = "insert into projects (project_name, billable_rate) " +
-                "values(@project_nameValue, @billable_rateValue); SELECT LAST_INSERT_ID();";
-            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-            //sqlCommand.Parameters.AddWithValue("@idNumValue", HttpUtility.UrlDecode(id));
-            sqlCommand.Parameters.AddWithValue("@project_nameValue", HttpUtility.UrlDecode(project_name));
-            sqlCommand.Parameters.AddWithValue("@billable_rateValue", HttpUtility.UrlDecode(billable_rate));
-            //this time, we're not using a data adapter to fill a data table.  We're just
-            //opening the connection, telling our command to "executescalar" which says basically
-            //execute the query and just hand me back the number the query returns (the ID, remember?).
-            //don't forget to close the connection!
-            sqlConnection.Open();
-            //we're using a try/catch so that if the query errors out we can handle it gracefully
-            //by closing the connection and moving on
-            try
-            {
-                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
-                //here, you could use this accountID for additional queries regarding
-                //the requested account.  Really this is just an example to show you
-                //a query where you get the primary key of the inserted row back from
-                //the database!
-            }
-            catch (Exception e)
-            {
-            }
-            sqlConnection.Close();
-        }
-
-        [WebMethod(EnableSession = true)]
-        public void AddTime(string project_name, string time_logged)
-        {
-            //string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-            string sqlConnectString = getConString();
-            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
-            //does is tell mySql server to return the primary key of the last inserted row.
-            string sqlSelect = "insert into users_times (project_name, time_logged) " +
-                "values(@project_nameValue, @time_loggedValue); SELECT LAST_INSERT_ID();";
-            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-            //sqlCommand.Parameters.AddWithValue("@idNumValue", HttpUtility.UrlDecode(id));
-            sqlCommand.Parameters.AddWithValue("@project_nameValue", HttpUtility.UrlDecode(project_name));
-            sqlCommand.Parameters.AddWithValue("@time_loggedValue", HttpUtility.UrlDecode(time_logged));
-            //this time, we're not using a data adapter to fill a data table.  We're just
-            //opening the connection, telling our command to "executescalar" which says basically
-            //execute the query and just hand me back the number the query returns (the ID, remember?).
-            //don't forget to close the connection!
-            sqlConnection.Open();
-            //we're using a try/catch so that if the query errors out we can handle it gracefully
-            //by closing the connection and moving on
-            try
-            {
-                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
-                //here, you could use this accountID for additional queries regarding
-                //the requested account.  Really this is just an example to show you
-                //a query where you get the primary key of the inserted row back from
-                //the database!
-            }
-            catch (Exception e)
-            {
-            }
-            sqlConnection.Close();
-        }
 
         [WebMethod(EnableSession = true)]
         public bool LogOff()
@@ -215,77 +146,6 @@ namespace ProjectTemplate
             return true;
         }
 
-        [WebMethod(EnableSession = true)]
-        public string GetProject()
-        {
-            try
-            {
-                string testQuery = "select * from projects";
-                ////////////////////////////////////////////////////////////////////////
-                ///here's an example of using the getConString method!
-                ////////////////////////////////////////////////////////////////////////
-                MySqlConnection con = new MySqlConnection(getConString());
-                ////////////////////////////////////////////////////////////////////////
-                MySqlCommand cmd = new MySqlCommand(testQuery, con);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-
-                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-                Dictionary<string, object> row;
-                foreach (DataRow dr in table.Rows)
-                {
-                    row = new Dictionary<string, object>();
-                    foreach (DataColumn col in table.Columns)
-                    {
-                        row.Add(col.ColumnName, dr[col]);
-                    }
-                    rows.Add(row);
-                }
-                return serializer.Serialize(rows);
-            }
-            catch (Exception e)
-            {
-                return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
-            }
-        }
-
-        [WebMethod(EnableSession = true)]
-        public string GetUserTime()
-        {
-            try
-            {
-                string testQuery = "select * from user_times where user_id = " + userID;
-                ////////////////////////////////////////////////////////////////////////
-                ///here's an example of using the getConString method!
-                ////////////////////////////////////////////////////////////////////////
-                MySqlConnection con = new MySqlConnection(getConString());
-                ////////////////////////////////////////////////////////////////////////
-                MySqlCommand cmd = new MySqlCommand(testQuery, con);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-
-                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-                Dictionary<string, object> row;
-                foreach (DataRow dr in table.Rows)
-                {
-                    row = new Dictionary<string, object>();
-                    foreach (DataColumn col in table.Columns)
-                    {
-                        row.Add(col.ColumnName, dr[col]);
-                    }
-                    rows.Add(row);
-                }
-                return serializer.Serialize(rows);
-            }
-            catch (Exception e)
-            {
-                return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
-            }
-        }
 
         [WebMethod(EnableSession = true)]
         public string GetUsers()
@@ -323,78 +183,6 @@ namespace ProjectTemplate
             }
         }
 
-        [WebMethod(EnableSession = true)]
-        public string GetTime()
-        {
-            try
-            {
-                string testQuery = "select user_times.project_name, user_times.time_logged, users.firstname from user_times, users where user_id = userid";
-                ////////////////////////////////////////////////////////////////////////
-                ///here's an example of using the getConString method!
-                ////////////////////////////////////////////////////////////////////////
-                MySqlConnection con = new MySqlConnection(getConString());
-                ////////////////////////////////////////////////////////////////////////
-                MySqlCommand cmd = new MySqlCommand(testQuery, con);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-
-                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-                Dictionary<string, object> row;
-                foreach (DataRow dr in table.Rows)
-                {
-                    row = new Dictionary<string, object>();
-                    foreach (DataColumn col in table.Columns)
-                    {
-                        row.Add(col.ColumnName, dr[col]);
-                    }
-                    rows.Add(row);
-                }
-                return serializer.Serialize(rows);
-            }
-            catch (Exception e)
-            {
-                return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
-            }
-        }
-
-
-        [WebMethod(EnableSession = true)]
-        public string GetTotalTime()
-        {
-            try
-            {
-                string testQuery = "select time_logged from user_times";
-                ////////////////////////////////////////////////////////////////////////
-                ///here's an example of using the getConString method!
-                ////////////////////////////////////////////////////////////////////////
-                MySqlConnection con = new MySqlConnection(getConString());
-                ////////////////////////////////////////////////////////////////////////
-                MySqlCommand cmd = new MySqlCommand(testQuery, con);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-
-                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-                Dictionary<string, object> row;
-                foreach (DataRow dr in table.Rows)
-                {
-                    row = new Dictionary<string, object>();
-                    foreach (DataColumn col in table.Columns)
-                    {
-                        row.Add(col.ColumnName, dr[col]);
-                    }
-                    rows.Add(row);
-                }
-                return serializer.Serialize(rows);
-            }
-            catch (Exception e)
-            {
-                return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
-            }
-        }
 
         [WebMethod(EnableSession = true)]
         public void CreateNewGoal(string goal)
@@ -509,7 +297,6 @@ namespace ProjectTemplate
         //[WebMethod(EnableSession = true)]
         //public Account[] searchResults(string searchInput)
         //{
-        //    DataTable sqlDt = new DataTable("accounts");
 
         //    string sqlQuery = "select userid, firstname, lastname, mentorshiptype from users where name=@searchInputValue";
 
